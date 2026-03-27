7ef7c00e1d81977ac1655691b2658bc7cf41ce7a
Hasan <robiulhasanjisan88@gmail.com>
2026-03-27 15:51:42 +0600

    TUPLE

--- TUPLE/p10.py
+++
@@ -1,5 +0,0 @@
-t = (5, 2, 9, 1, 7)
-
-
-t_asc = tuple(sorted(t))          # ascending
-t_desc = tuple(sorted(t, reverse=True))  # descending
--- TUPLE/p4.py
+++
@@ -1,5 +0,0 @@
-# Count how many times an element appears in a tuple
-
-n = tuple(input("Enter 5 numbers: ").split())  # for string
-n = tuple(map(int,input("Enter 5 numbers: ").split())) # for int 
-print(n.count("jis"))
